a0156dde2954dcb2c1a76a65302f17ffee1eb2c5
Yusuf720 <56973730+Yusuf720@users.noreply.github.com>
2024-08-13 10:30:26 +0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- * Akbarali yozgan OOP dagi kod
- * sana 23.10.2020
- * Website: UzHackerSW.uz
- * Namuna: https://uzhackersw.uz/modul/clashofclans/
- * Akbarali bilan bog`lanish; Email: Akbarali@uzhackersw.uz
-*/
-define("TOKEN_CLASH_OF_CLANS", 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjJiOTBkODViLTAxNzAtNDg3OS1iNDVlLWExNDliZjdlZmY1MCIsImlhdCI6MTcyMzEyNzE3Nywic3ViIjoiZGV2ZWxvcGVyL2UzZmZjOTdjLTkzNDItMzQxZi1kY2ZiLWI4YzY5MTU3NTQ3OSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjMxLjE4Ni4xMS4xMTMiXSwidHlwZSI6ImNsaWVudCJ9XX0.FwxMBOWF3tz9V5SH6ChNfv7JlEzhdtQ3H4xe9DDhwkgfmSzf-h63ammFtGUBzxD2bmVAWF88uYVz8N3i4gEBXw'); // Clash of clans kirish tokeni uni https://developer.clashofclans.com/#/login dan oling
